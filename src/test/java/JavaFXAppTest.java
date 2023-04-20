4488359049630dfff6d3c32263721b9fbef4d698
Sven Dobbe <dobbe.sven@hotmail.nl>
2023-04-20 13:45:45 +0200

    working + test

--- src/test/java/JavaFXAppTest.java
+++ src/test/java/JavaFXAppTest.java
@@ -6,4 +6,20 @@ class JavaFXAppTest {
     void aDummyTest() {
         assertEquals(0,0);
     }
+
+    @Test
+    void computeAddTest() {
+        //Arrange
+        int num1 = 3;
+        int num2 = 4;
+        int expected = 7;
+        JavaFXApp SOT = new JavaFXApp();
+
+        //Act
+        int result = SOT.computeAdd(num1, num2);
+
+        //Assert
+        assertEquals(expected, result);
+
+    }
 }
